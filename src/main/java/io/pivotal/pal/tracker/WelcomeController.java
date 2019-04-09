ac58083ad7a9141f2c90a4f64aba9cf946f591cf
karthikchennai1 <karthikeyan.t44@wipro.com>
2019-04-08 17:38:39 -0700

    Lap-2 commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,26 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class WelcomeController {
+
+
+    private String welcomeMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
     @GetMapping("/")
     public String getStrings(){
-        return "hello";
+        return welcomeMessage;
     }
+
+
 }
